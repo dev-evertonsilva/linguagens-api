bd239aaf6192469f21187f6a26585c8c570be2fa
Everton Silva <everton.ss7@icloud.com>
2022-07-25 16:06:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/estudo/linguagens/api/Linguagem.java
@@ -0,0 +1,37 @@
+package br.com.estudo.linguagens.api;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
+public class Linguagem {
+
+    @Id
+    private String id;
+    private String title;
+    private String image;
+    private int rankings;
+
+    public Linguagem() {
+
+    }
+    
+    public Linguagem(String title, String image, int rankings) {
+        this.title = title;
+        this.image = image;
+        this.rankings = rankings;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public int getRankings() {
+        return rankings;
+    }
+
+    
+}
